cbe4e14a8acba27a8fd4fb8fd92aceabf894233c
Valentin <Chris_valentin@hotmail.com>
2023-04-26 18:28:10 +0200

    done (minus after effects)?

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -24,6 +24,11 @@ var x = setInterval(function () {
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
+  days = days < 10 ? "0" + days : days;
+  hours = hours < 10 ? "0" + hours : hours;
+  minutes = minutes < 10 ? "0" + minutes : minutes;
+  seconds = seconds < 10 ? "0" + seconds : seconds;
+
   document.getElementById("days").innerHTML = days;
   document.getElementById("hours").innerHTML = hours;
   document.getElementById("minutes").innerHTML = minutes;
