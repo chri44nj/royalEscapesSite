1617dacd0bd69fb34640799807135d32567f122c
Valentin <Chris_valentin@hotmail.com>
2023-04-26 14:52:18 +0200

    web done (minus footer)

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,3 +1,19 @@
+const burgerMenu = document.querySelector(".burgerMenu");
+const navItems = document.querySelector(".navItems");
+
+burgerMenu.addEventListener("mousedown", () => {
+  burgerMenu.classList.toggle("active");
+  navItems.classList.toggle("active");
+  console.log("Hey fisse!");
+});
+
+document.querySelectorAll(".navItem").forEach((n) =>
+  n.addEventListener("mousedown", () => {
+    burgerMenu.classList.remove("active");
+    navItems.classList.remove("active");
+  })
+);
+
 var countdownDate = new Date("May 4, 2023 00:00:00").getTime();
 var x = setInterval(function () {
   var now = new Date().getTime();
